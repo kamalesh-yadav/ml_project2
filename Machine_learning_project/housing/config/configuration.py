10ab382e027a3150270ab577a435ae8b53ccb3fc
kamalesh <studentpw2@gmail.com>
2025-12-23 11:25:15 +0530

    updated

---
+++ Machine_learning_project/housing/config/configuration.py
@@ -0,0 +1,29 @@
+from entity.config_entity import DataIngestionConfig,DataValidationConfig,DataTransformationConfig
+from entity.config_entity import ModelTrainerConfig,ModelPusherConfig,ModelEvaluationConfig,TrainingPipelineConfig
+from util.util import read_yaml_file
+import sys,os
+from housing.constant import *
+from housing.exception import HousingException
+class Configuration:
+    def __init__(self,
+                 config_file_path:str=CONFIG_FILE_PATH,
+                 current_time_stamp:str=CURRENT_TIME_STAMP)->None:
+        self.config_info = read_yaml_file(file_path=config_file_path)
+        self.training_pipeline_config=self.get_training_pipeline_config() #Going to call the fn directly
+        self.training_pipeline_config.artifact_dir #due to namedtuple we are getting suggestions wwc
+        self.time_stamp = current_time_stamp
+        pass
+    def get_data_ingestion_config(self)->DataIngestionConfig: # will return an entity
+        pass
+    def get_data_validation_config(self)->DataValidationConfig:
+        pass
+    def get_data_transformation_config(self)->DataTransformationConfig:
+        pass
+    def get_model_trainer_config(self)->ModelTrainerConfig:
+        pass
+    def get_model_evaluation_config(self)->ModelEvaluationConfig:
+        pass
+    def get_model_pusher_config(self)->ModelPusherConfig:
+        pass
+    def get_training_pipeline_config(self)->TrainingPipelineConfig:
+        pass
